ce9c35e331a1e2e72c483a6d6a327ff239aa63fd
Asher Pivko <apivko@gmail.com>
2025-06-02 21:23:19 +0300

    Initial commit

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+// vite.config.js
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+export default defineConfig({
+  base: 'Hashozer2', // Replace with your GitHub repo name
+  plugins: [vue()],
+})
